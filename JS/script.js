2b9627f7d13f3a7356888403e8c77021e3d3e0b6
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-10 17:16:20 +0100

    added event listner on button

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,31 @@
-console.log('HELLO WORLD');
+
+//# RECUPERO GLI ELEMENTI
+
+const gridElement = document.querySelector('.grid');
+
+const button = document.querySelector('button');
+
+const rows = 10;
+const cols = 10;
+const totalCell = rows * cols;
+
+//CREO EVENT LISTNER
+button.addEventListener('click', function(){
+    
+    const cellGenerator = () =>{
+        for(let i = 1; i<= totalCell;i++){
+            //CREO ELEMENTO CELLA
+            const cellElement =document.createElement('div');
+            // GLI DO LA CLASSE
+            cellElement.classList.add('cell');
+            // AGGANGIO ALLA GRIGLIA
+            gridElement.appendChild(cellElement);
+        }
+        }
+        cellGenerator()
+
+
+
+
+})
+
