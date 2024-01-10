137ce8fcd7732362039b143c5d3c305517c9fade
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-10 23:22:35 +0100

    added condition for event listner

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,11 @@
 
 //# FUNZIONI
-const cellGenerator = () =>{
+const cellGenerator = (content) =>{
     //CREO ELEMENTO CELLA
     const cellElement =document.createElement('div');
     // GLI DO LA CLASSE
     cellElement.classList.add('cell');
+    cellElement.innerText = content;
     return cellElement
 }
 
@@ -16,24 +17,32 @@ const button = document.querySelector('button');
 const rows = 10;
 const cols = 10;
 const totalCell = rows * cols;
+let hasCells = false;
 
 //CREO EVENT LISTNER SUL BOTTONE
 button.addEventListener('click', function(){
-    for(let i = 1; i <= totalCell; i++){
-        //CREO LA CELLA
-        const cellElement = cellGenerator();
-        // AGGANGIO LA CELLA ALLA GRIGLIA
-        gridElement.appendChild(cellElement);
-        // AGGIUNGO IL NUMERO ALLA CELLA
-        cellElement.innerText = i;
-        // CREO EVENT LISTNER SULLA CELLA
-        cellElement.addEventListener('click',function(){
-            //AGGIUNGO IL NUMERO ALLA CELLA
-            console.log(i)
-            // COLORO LA CELLA DI BLU AL CLICK
-            cellElement.classList.add('clicked')
-            console.log(cellElement);
-        })
+    if(hasCells){ return;
+    }else{
+        for(let i = 1; i <= totalCell; i++){
+            //CREO LA CELLA
+            let cellElement = cellGenerator(i);
+            console.log('sono la cella', cellElement)
+    
+            // AGGANGIO LA CELLA ALLA GRIGLIA
+            gridElement.appendChild(cellElement);
+    
+            // AGGIUNGO IL NUMERO ALLA CELLA
+            cellElement.innerText = i;
+            
+            hasCells = true
+            // CREO EVENT LISTNER SULLA CELLA
+            cellElement.addEventListener('click', function(){
+                console.log('SONO LA CELLA', cellElement);
+    
+                // COLORO LA CELLA DI BLU AL CLICK
+                cellElement.classList.add('clicked');
+            })
+        }
     }
 })
 
