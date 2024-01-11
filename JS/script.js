f293ab04e5fd94985015ed1190fdabf45df85714
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-11 15:45:50 +0100

    added eventListner on form

--- JS/script.js
+++ JS/script.js
@@ -9,18 +9,28 @@ const cellGenerator = () =>{
 }
 
 //# RECUPERO GLI ELEMENTI
+const formElement = document.querySelector('form')
+const selectElement = document.querySelector('select')
+console.log(formElement,selectElement)
+const button = document.querySelector('button');
+
+
+
 const gridElement = document.querySelector('.grid');
 
-const button = document.querySelector('button');
 
 const rows = 10;
 const cols = 10;
 const totalCell = rows * cols;
 let hasCells = false;
+const mode = selectElement.value;
 
 //CREO EVENT LISTNER SUL BOTTONE
-button.addEventListener('click', function(){
-    if(hasCells){ return;
+formElement.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    if(hasCells){
+        return;
     }else{
         for(let i = 1; i <= totalCell; i++){
             //CREO LA CELLA
@@ -33,11 +43,14 @@ button.addEventListener('click', function(){
             cellElement.innerText = i;
             
             hasCells = true
+
             // CREO EVENT LISTNER SULLA CELLA
             cellElement.addEventListener('click', function(){
-                
-                //STAMPO IN CONSOLE IL NUMERO DELLA CELLA
-                console.log('sono la cella', i)
+                //EVITO CHE LO STAMPI SE GIA E' STATA CLICCATA
+                if(!cellElement.classList.contains('clicked')){
+                    //STAMPO IN CONSOLE IL NUMERO DELLA CELLA
+                    console.log('sono la cella', i)
+                }else return;
     
                 // COLORO LA CELLA DI BLU AL CLICK
                 cellElement.classList.add('clicked');
